f97910260284756c1d872bdc1b5c7c5a74cc8a58
Joakim Anker Kruse <joakimkruse@hotmail.com>
2022-11-27 02:58:18 +0100

    More comments

--- app/src/main/java/com/example/wheeloffortune/ui/elements/GameStatus.kt
+++ app/src/main/java/com/example/wheeloffortune/ui/elements/GameStatus.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.sp
 
+//Composable that shows the user's life and score.
 @Composable
 fun GameStatus(life: Int, score: Int) {
     Text(
